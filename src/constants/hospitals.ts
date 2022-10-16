3bc42c8b9d8df0da590bcac87133396471549fc0
abUwUser <pedro.santos.cartaxo@gmail.com>
2022-10-16 13:33:39 -0300

    OKAY LETS FINISH THIS HOMEWORK

--- src/constants/hospitals.ts
+++ src/constants/hospitals.ts
@@ -1,12 +1,102 @@
 export default [
     {
-        name: "Hospital 1",
-        icon: "https://www.colegioweb.com.br/wp-content/uploads/2016/10/Dia-do-Hospital-1.jpg",
-        type: "private",
-        coords: [-15.730069, -47.868752],
+        name: "Hospital da criança José de Alencar",
+        icon: "https://www.hcb.org.br/arquivos/img_1482259500.jpg",
+        type: "public",
+        coords: [-15.75912, -47.91729],
         eta: 1.8e+6,
         peopleWaiting: 22,
-        attendancePerHour: 44,
+        attendancePerHour: 46,
+
+        hasICU: true,
+        hasAvaiableSurgeries: true,
+
+        targetAudience: {
+            children: true,
+            adults: false,
+            seniors: false,
+        },
+
+        urgencyAttendance: {
+            emergency: true,
+            veryUrgent: true,
+            urgent: true,
+            littleUrgent: true,
+            notUrgent: true
+        },
+
+        equipaments: {
+            defibrillators: true,
+            anesthesiaMachine: true,
+            breathing: true
+        },
+
+        doctors: {
+            dentist: false,
+            dermatologist: true,
+            gynecologist: false,
+            infectologist: true,
+            neurologist: true,
+            ophthalmologist: false,
+            oncologist: true,
+            orthopedist: true,
+            psychiatrist: true,
+            radiologist: true
+        }
+    },
+    {
+        name: "Hospital Santa Luzia",
+        icon: "http://hsl.rededor.com.br/images/logo/santaluzia.png",
+        type: "private",
+        coords: [-15.82724, -47.93035],
+        eta: 2.28e+6,
+        peopleWaiting: 31,
+        attendancePerHour: 42,
+
+        hasICU: true,
+        hasAvaiableSurgeries: true,
+
+        targetAudience: {
+            children: true,
+            adults: true,
+            seniors: true,
+        },
+
+        urgencyAttendance: {
+            emergency: true,
+            veryUrgent: true,
+            urgent: true,
+            littleUrgent: true,
+            notUrgent: true
+        },
+
+        equipaments: {
+            defibrillators: true,
+            anesthesiaMachine: true,
+            breathing: true
+        },
+
+        doctors: {
+            dentist: true,
+            dermatologist: true,
+            gynecologist: true,
+            infectologist: true,
+            neurologist: true,
+            ophthalmologist: true,
+            oncologist: true,
+            orthopedist: true,
+            psychiatrist: true,
+            radiologist: true
+        }
+    },
+    {
+        name: "Hospital Santa Helena",
+        icon: "https://www.agenciafull.com.br/wp-content/uploads/2011/03/hospital-santa-helena-logomarca.jpg",
+        type: "private",
+        coords: [-15.73582, -47.896967],
+        eta: 2.46e+6,
+        peopleWaiting: 12,
+        attendancePerHour: 18,
 
         hasICU: true,
         hasAvaiableSurgeries: true,
@@ -45,20 +135,65 @@ export default [
         }
     },
     {
-        name: "Hospital 2",
-        icon: "https://thumbs.dreamstime.com/b/edif%C3%ADcio-do-hospital-59693686.jpg",
+        name: "HRAN",
+        icon: "https://midias.correiobraziliense.com.br/_midias/jpg/2021/03/10/675x450/1_pri_1103_1901_cor_18cm_g-6562649.jpg",
         type: "public",
-        coords: [-15.758458, -47.940946],
-        eta: 2.7e+6,
-        peopleWaiting: 31,
-        attendancePerHour: 29,
+        coords: [-15.78493, -47.88286],
+        eta: 3.3e+6,
+        peopleWaiting: 52,
+        attendancePerHour: 60,
 
         hasICU: true,
         hasAvaiableSurgeries: true,
 
         targetAudience: {
             children: true,
-            adults: false,
+            adults: true,
+            seniors: true,
+        },
+
+        urgencyAttendance: {
+            emergency: true,
+            veryUrgent: true,
+            urgent: true,
+            littleUrgent: true,
+            notUrgent: true
+        },
+
+        equipaments: {
+            defibrillators: true,
+            anesthesiaMachine: true,
+            breathing: true
+        },
+
+        doctors: {
+            dentist: false,
+            dermatologist: true,
+            gynecologist: true,
+            infectologist: true,
+            neurologist: false,
+            ophthalmologist: true,
+            oncologist: false,
+            orthopedist: false,
+            psychiatrist: true,
+            radiologist: true
+        }
+    },
+    {
+        name: "HMIB",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Hospital_Materno_Infantil_%2848991222802%29.jpg/1920px-Hospital_Materno_Infantil_%2848991222802%29.jpg",
+        type: "public",
+        coords: [-15.82348, -47.89722],
+        eta: 3.72e+6,
+        peopleWaiting: 31,
+        attendancePerHour: 30,
+
+        hasICU: true,
+        hasAvaiableSurgeries: true,
+
+        targetAudience: {
+            children: true,
+            adults: true,
             seniors: false,
         },
 
@@ -88,5 +223,50 @@ export default [
             psychiatrist: false,
             radiologist: true
         }
-    }
+    },
+    {
+        name: "Hospital Santa Lúcia SUL",
+        icon: "https://scontent.fbsb3-1.fna.fbcdn.net/v/t39.30808-6/309449423_2054962914712220_4325920184549803649_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=MUYzhQ6YlLYAX_fY1-m&_nc_ht=scontent.fbsb3-1.fna&oh=00_AT8utvr841KowLY8Wa_PDggqdRQEnuwvQ_c8cGsHOc4f1A&oe=63506527",
+        type: "private",
+        coords: [-15.82817, -47.92980],
+        eta: 4.32e+6,
+        peopleWaiting: 80,
+        attendancePerHour: 73,
+
+        hasICU: true,
+        hasAvaiableSurgeries: true,
+
+        targetAudience: {
+            children: true,
+            adults: true,
+            seniors: true,
+        },
+
+        urgencyAttendance: {
+            emergency: true,
+            veryUrgent: true,
+            urgent: true,
+            littleUrgent: true,
+            notUrgent: true
+        },
+
+        equipaments: {
+            defibrillators: true,
+            anesthesiaMachine: true,
+            breathing: true
+        },
+
+        doctors: {
+            dentist: false,
+            dermatologist: false,
+            gynecologist: true,
+            infectologist: true,
+            neurologist: true,
+            ophthalmologist: false,
+            oncologist: true,
+            orthopedist: true,
+            psychiatrist: false,
+            radiologist: true
+        }
+    },
 ] as unknown as Hospital[]
